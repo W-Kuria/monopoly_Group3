042353525ecf36300693d97962ba31d294c708e9
Mathew <mathewkariuki2005@gmail.com>
2025-07-23 16:22:28 +0300

    added three.jsx

---
+++ src/dice.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function Dice({Roll}) {
+  const [number1, setNumber1] = useState(null);
+  const [number2, setNumber2] = useState(null);
+
+  const randomNumber = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const generate = () => {
+    const newNumber1=randomNumber(1,6)
+    const newNumber2=randomNumber(1,6)
+    setNumber1(newNumber1);
+    setNumber2(newNumber2)
+   const sum=newNumber1+newNumber2;
+  alert(`move your piece+ ${sum}`)
+  if(Roll){
+    Roll(sum)
+  }
+  };
+
+  
+
+  return (
+    <div>
+      <button onClick={generate}>Roll Dice</button>
+    </div>
+  );
+}
+
+export default Dice;
